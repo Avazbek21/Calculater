873771b25665d9908475a1747573762b967ab7de
Avazbek21 <avazbek2158@gmail.com>
2025-05-14 02:49:39 +0300

    Add files via upload

---
+++ Калькулятор на сайте/functions.js
@@ -0,0 +1,46 @@
+function num1(){ 
+	var num = document.getElementById("inp1").value;
+	isNaN(num)
+		alert("Введите число!");
+	 		
+		document.getElementById("print").value = String(document.getElementById("print").value+num;
+}
+function num2(){ 
+	var num = document.getElementById("inp2").value;
+	document.getElementById("print").value = num;
+}
+function num3(){ 
+	var num = document.getElementById("inp3").value;
+	document.getElementById("print").value = num;
+}
+function num4(){ 
+	var num = document.getElementById("inp4").value;
+	document.getElementById("print").value = num;
+}
+function num5(){ 
+	var num = document.getElementById("inp5").value;
+	document.getElementById("print").value = num;
+}
+function num6(){ 
+	var num = document.getElementById("inp6").value;
+	document.getElementById("print").value = num;
+}
+function num7(){ 
+	var num = document.getElementById("inp7").value;
+	document.getElementById("print").value = num;
+}
+function num8(){ 
+	var num = document.getElementById("inp8").value;
+	document.getElementById("print").value = num;
+}
+function num9(){ 
+	var num = document.getElementById("inp9").value;
+	document.getElementById("print").value = num;
+}
+function num0(){ 
+	var num = document.getElementById("inp0").value;
+	document.getElementById("print").value = num;
+}
+function numClear(){
+	document.getElementById("print").value = "";
+}
